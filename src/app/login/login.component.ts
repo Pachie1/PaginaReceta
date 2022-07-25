6f69cfff25dc130662e16347c62a6c973443000f
Gaspar <gasparorma13@gmail.com>
2022-07-25 15:18:01 -0300

    parche login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,22 +23,25 @@ export class LoginComponent implements OnInit {
 
 
   login(){
-    if (this.username.trim().length === 0){
-      this.errorMsg = "Username is required";
-    } else if (this.password.trim().length === 0){
-      this.errorMsg = "Password is required";
-    } else{
-      this.errorMsg = "";
-      // Llamo al servicios auth para verficar el username y el password
-      let res = this.auth.login(this.username, this.password);
-      // si la respuesta fue el codigo 200 avanzo a la pagina home
-      if (res === 200){
-        this.router.navigate(['home']);
-      }
-      // si la respuesta fue 403 se cambia en mensaje de error
-      if (res === 403){
-        this.errorMsg = "Invalid Credentials";
-      }
-    }
+    this.router.navigate(['home']);
   }
+  // login(){
+  //   if (this.username.trim().length === 0){
+  //     this.errorMsg = "Username is required";
+  //   } else if (this.password.trim().length === 0){
+  //     this.errorMsg = "Password is required";
+  //   } else{
+  //     this.errorMsg = "";
+  //     // Llamo al servicios auth para verficar el username y el password
+  //     let res = this.auth.login(this.username, this.password);
+  //     // si la respuesta fue el codigo 200 avanzo a la pagina home
+  //     if (res === 200){
+  //       this.router.navigate(['home']);
+  //     }
+  //     // si la respuesta fue 403 se cambia en mensaje de error
+  //     if (res === 403){
+  //       this.errorMsg = "Invalid Credentials";
+  //     }
+  //   }
+  // }
 }
